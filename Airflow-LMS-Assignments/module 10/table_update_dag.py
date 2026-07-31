c39846949f889d108386dcf2774a0810504058d2
SARAS-GD <srajuv@griddynamics.com>
2026-07-31 15:27:00 +0530

    Update table_update_dag.py

--- Airflow-LMS-Assignments/module 10/table_update_dag.py
+++ Airflow-LMS-Assignments/module 10/table_update_dag.py
@@ -15,7 +15,7 @@ with DAG(
     dag_id='table_update_dag',
     default_args=default_args,
     description='A simple DAG for table updates',
-    schedule_interval=None,
+    schedule=None,
     start_date=datetime(2024, 6, 1),
     catchup=False,
 ) as dag:
